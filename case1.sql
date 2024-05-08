331a78aaf4f66144a4d660fb0e2835d58920b7f8
Ahmed Khaled Elsharabasy <90667920+ahmedsharabasy@users.noreply.github.com>
2024-05-08 08:11:24 +0300

    Update case1.sql

--- case1.sql
+++ case1.sql
@@ -62,7 +62,7 @@ group by s.customer_id;
 
 --How many days has each customer visited the restaurant?
 
-select customer_id, count(customer_id ) from sales
+select count(distinct(order_date )) from sales
 group by customer_id;
 
 
